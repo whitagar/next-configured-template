171ff77f3ea5dfd17170d7db1a00b3e978af6710
Garrison Whitaker <garrison.whitaker@neighbor.com>
2022-08-12 12:29:27 -0600

    Initial next template

---
+++ tailwind.config.js
@@ -0,0 +1,10 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  mode: 'jit',
+  purge: ['./pages/**/*.{js,jsx,ts,tsx}'],
+  content: ['./**/*.{js,ts,jsx,tsx}'],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
